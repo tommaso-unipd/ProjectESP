452add5d1e37dba54a6cc2069d1009a619554e93
tommaso-unipd <tommaso.pasquetto@studenti.unipd.it>
2026-04-19 17:27:44 +0200

    creato risorse e la visione verticale della schermata 1

---
+++ README.md
@@ -0,0 +1,12 @@
+DISPOSITIVO UTILIZZATO
+ho utilizzato l'emulatore del Pixel 2 Android 16.0
+
+
+SVOLGIMENTO 
+
+Per prima cosa ho creato le Risorse (colori e testi) in 3 file xml; 2 file per le stringhe(it e eng)
+e uno per i 6 colori oltre al bianco e il nero.
+
+poi ho creato i due file activity_main xml per la schermata 1 (uno per la visione verticale e uno
+per quella orizzontale)
+
--- app/src/main/java/com/example/projectesp/MainActivity.kt
+++ app/src/main/java/com/example/projectesp/MainActivity.kt
@@ -14,34 +14,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.projectesp.ui.theme.ProjectESPTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ProjectESPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjectESPTheme {
-        Greeting("Android")
-    }
-}
